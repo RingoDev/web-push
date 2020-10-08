41551887e54ec89f00d066b22c5cf90d52535def
ringodev <ringodev@protonmail.com>
2020-10-08 23:02:54 +0200

    added CORS for ringodev.com

--- src/main/java/com/ringodev/webpush/SubscriptionController.java
+++ src/main/java/com/ringodev/webpush/SubscriptionController.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,6 +14,7 @@ public class SubscriptionController {
 
     private final Logger logger = LoggerFactory.getLogger(SubscriptionController.class);
 
+    @CrossOrigin(origins="https://www.ringodev.com")
     @PostMapping("/add")
     public ResponseEntity<Object> addSubscription(@RequestBody Subscription subscription, HttpServletRequest request){
         logger.info("Tried to add Subscription");
