a8280aa51e437a1c5395f082c4df9141a5191443
ringodev <ringodev@protonmail.com>
2020-10-09 14:37:44 +0200

    working commit

--- src/main/java/com/ringodev/webpush/WebPushService.java
+++ src/main/java/com/ringodev/webpush/WebPushService.java
@@ -1,7 +1,6 @@
 package com.ringodev.webpush;
 
 
-
 import nl.martijndwars.webpush.Notification;
 import nl.martijndwars.webpush.PushService;
 import org.bouncycastle.openssl.PEMKeyPair;
@@ -15,13 +14,16 @@ import org.springframework.stereotype.Service;
 import java.io.*;
 import java.security.GeneralSecurityException;
 import java.security.KeyPair;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class WebPushService {
 
 
-    /** The Time to live of GCM notifications */
+    /**
+     * The Time to live of GCM notifications
+     */
     private static final int TTL = 255;
 
     private final Logger logger = LoggerFactory.getLogger(WebPushService.class);
@@ -30,39 +32,26 @@ public class WebPushService {
 
 
         // Figure out if we should use GCM for this notification somehow
-        boolean useGcm = false;
         Notification notification;
         PushService pushService;
 
-        if (!useGcm) {
-            // Create a notification with the endpoint, userPublicKey from the subscription and a custom payload
-            notification = new Notification(
-                    sub.getEndpoint(),
-                    sub.getUserPublicKey(),
-                    sub.getAuthAsBytes(),
-                    payload
-            );
+        // Create a notification with the endpoint, userPublicKey from the subscription and a custom payload
+        notification = new Notification(
+                sub.getEndpoint(),
+                sub.getUserPublicKey(),
+                sub.getAuthAsBytes(),
+                payload
+        );
 
-            // Instantiate the push service, no need to use an API key for Push API
-            pushService = new PushService().setKeyPair(getKeys());
+        // Instantiate the push service, no need to use an API key for Push API
+        pushService = new PushService().setKeyPair(getKeys());
 
-        } else {
-            // Or create a GcmNotification, in case of Google Cloud Messaging
-            notification = new Notification(
-                    sub.getEndpoint(),
-                    sub.getUserPublicKey(),
-                    sub.getAuthAsBytes(),
-                    payload,
-                    TTL
-            );
-
-            // Instantiate the push service with a GCM API key
-            pushService = new PushService("gcm-api-key");
-        }
 
         // Send the notification
-        logger.info("Sending Notification");
-        logger.info(notification.toString());
+        logger.info("Sending Notification to");
+        logger.info(notification.getEndpoint());
+        logger.info(notification.getUserPublicKey().toString());
+        logger.info(Arrays.toString(notification.getPayload()));
         pushService.send(notification);
     }
 
